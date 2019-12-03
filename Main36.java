86efbed6296a066d45329acfbdd4796f82eeec43
Mbabysbreath <1830817543@qq.com>
2019-12-03 14:44:59 +0800

    OJ练习

---
+++ Main36.java
@@ -0,0 +1,30 @@
+package nowCode;
+
+import java.util.List;
+
+/**
+ * 链表相加
+ * @author ZhaoMin
+ * @date 2019/12/1 15:11
+ */
+class ListNode{
+    int val;
+    ListNode next=null;
+    ListNode(int val){
+        this.val=val;
+    }
+}
+public class Main36 {
+    /*public ListNode plusAB(ListNode a, ListNode b) {
+        // write code here
+        ListNode list=new ListNode(0);
+        ListNode cur1=a;
+        ListNode cur2=b;
+        while(cur1!=null&&cur2!=null){
+            list.val=cur1.val+cur2.val;
+            ListNode node=new ListNode(0);
+          //  list
+        }
+
+    }*/
+}
